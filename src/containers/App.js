e21090eef552f8addc192d582ef16e6bf9a6d9c5
ccsombokjr <ccsombokjr@gmail.com>
2019-04-11 19:12:36 +0100

    changed codebase to work with redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,47 +1,54 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
+import { setSearchField, requestRobots } from '../actions';
 
-class App extends Component {
-	constructor() {
-		super()
-		this.state = {
-			Robots: [],
-			searchfield: ''
-		}
-	}
-
-	componentDidMount() {
-		fetch('https://jsonplaceholder.typicode.com/users')
-			.then(response => response.json())
-			.then(users => this.setState({ Robots: users }));
-	}
-
-	onSearchChange = (event) => {
-		this.setState({ searchfield: event.target.value })
-	}
-
-	render() {
-		const { Robots, searchfield } = this.state;
-		const filteredRobots = Robots.filter(robot => {
-			return robot.name.toLowerCase().includes(searchfield.toLowerCase())
-		})
-		return (
-			<div className='tc'>
-				<h1 className='f1'> RoboFriends </h1>
-				<SearchBox searchChange={this.onSearchChange} />
-				<Scroll>
-					<ErrorBoundry>
-						<CardList Robots = {filteredRobots} />
-					</ErrorBoundry>
-				</Scroll>
-			</div>
-		);
+const mapStateToProps = state => { 
+	return{
+		searchField: state.searchRobots.searchField,
+		robots: state.requestRobots.robots,
+		isPending: state.requestRobots.isPending,
+		error: state.requestRobots.error
 	}
 }
 
-export default App;
+const mapDispatchToRrops = (dispatch) => {
+	return { 
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value)), //onSearchChange here is a prop
+		onRequestRobots: () => dispatch(requestRobots()) //onRequestRobots is a prop here as well; same as dispatch(requestRobots()) but can be expressed like that due to redux-thunk
+	}
+}
+
+class App extends Component {
+
+	componentDidMount() {
+		this.props.onRequestRobots();
+	}
+
+	render() {
+		const { searchField, onSearchChange, robots, isPending } = this.props;
+		const filteredRobots = robots.filter(robot => {
+			return robot.name.toLowerCase().includes(searchField.toLowerCase())
+		})
+		return isPending ?
+		<h1>Loading</h1> :
+			(
+				<div className='tc'>
+					<h1 className='f1'> RoboFriends </h1>
+					<SearchBox searchChange={onSearchChange} />
+					<Scroll>
+						<ErrorBoundry>
+							<CardList Robots = {filteredRobots} />
+						</ErrorBoundry>
+					</Scroll>
+				</div>
+			);
+	}
+}
+
+export default connect(mapStateToProps, mapDispatchToRrops)(App); //connect() is a higher order function that returns a function; thus the syntax
--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import { createStore, applyMiddleware, combineReducers } from 'redux';  //creates store for states &&middleware betweeen actions and reducers && allows combination of multiple reducers 
+import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk'; //middleware for async processes using dispatch(); allows functions to be passed to actions and executed in the background by sending pending to reducer
+import { createLogger } from 'redux-logger';
+
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
+
 import 'tachyons';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { searchRobots, requestRobots } from './reducers';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const logger = createLogger();
+const rootReducer = combineReducers({ searchRobots, requestRobots })
+const store = 
+	createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>, document.getElementById('root'));
 serviceWorker.unregister();
